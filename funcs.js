f74cf9cfb939d4bb5896a93c20293fbc3f012cf3
Davor Kandic <markojank727@gmail.com>
2019-11-04 18:37:36 +0000

    url testing

--- funcs.js
+++ funcs.js
@@ -3,3 +3,9 @@ function openInNewTab() {
   var win = window.open("https://www.google.com", '_blank');
   win.focus();
 }
+
+
+function openInNewTab_profile() {
+  var win = window.open("https://www.google.com", '_blank');
+  win.focus();
+}
