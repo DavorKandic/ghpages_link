b3d7c2f7c324cbdc584022e591652b66a4635616
Davor Kandic <markojank727@gmail.com>
2019-11-04 22:17:15 +0000

    without func

--- funcs.js
+++ funcs.js
@@ -1,10 +1,4 @@
 
-function openInNewTab() {
-  var win = window.open("https://github.com/DavorKandic/ghpages_link/profile.html", '_blank');
-  win.focus();
-}
-
-
 function openInNewTab_profile() {
   var win = window.open("https://www.google.com", '_blank');
   win.focus();
